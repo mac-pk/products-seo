e086b27ae4d12292885a6421e56e22b4959d6c86
Sarmad Ali <sarmad.ali@asicentral.com>
2019-10-22 15:57:46 +0500

    Repo Updated

--- SEOWorkflow/Angular/src/environments/environment.prod.ts
+++ SEOWorkflow/Angular/src/environments/environment.prod.ts
@@ -2,6 +2,7 @@ export const environment = {
   production: true,
   apiBaseUrl: "https://productservice.stage-asicentral.com/api/v5/",
   mockBaseUrl: "http://www.mocky.io/",
-  seoBaseUrl: "http://localhost:54936/api/",
+  //seoBaseUrl: "http://localhost:54936/api/",
+  seoBaseUrl: "http://macpk-prfm2:8083/api/",
   seoBaseUrl1: "http://localhost:54937/api/supplier"
 };
